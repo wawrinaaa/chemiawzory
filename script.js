a0df9170dc1c00e53e04976ccce4fdf7046b87c4
wawrinaaa <57635661+wawrinaaa@users.noreply.github.com>
2020-08-15 18:33:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,164 @@
+let inside1='<div id="formula1" class="formula" onclick="showformula(1, 1)">Moment dipolowy</div><div id="formula2" class="formula" onclick="showformula(1, 2)">Masa próbki wytr. w skutek elektrolizy</div><div id="formula3" class="formula" onclick="showformula(1, 3)">Równoważnik Elektrochemiczny</div><div id="formula4" class="formula" onclick="showformula(1, 4)">Siła ele. motoryczna ogniwa (EMF)</div>';
+
+    let bf11='<span onmouseover="explanation(1, 1, 1);">&#181;</span>=<span onmouseover="explanation(1, 1, 2);">Q</span>*<span onmouseover="explanation(1, 1, 3);">r</span>';
+
+    let bf12='<span onmouseover="explanation(1, 2, 1);">m</span>=<span onmouseover="explanation(1, 2, 2);">k</span>*<span onmouseover="explanation(1, 2, 3);">i</span>*<span onmouseover="explanation(1, 2, 4);">t</span>';
+
+    let bf13='<span onmouseover="explanation(1, 3, 1);">k</span>=<span onmouseover="explanation(1, 3, 2);">M</span>/(<span onmouseover="explanation(1, 3, 3);">z</span>*<span onmouseover="explanation(1, 3, 4);">f</span>)';
+
+    let bf14='<span onmouseover="explanation(1, 4, 1);">EMF</span>=<span onmouseover="explanation(1, 4, 2);">E<sub>k</sub></span>-<span onmouseover="explanation(1, 4, 3);">E<sub>a</sub></span>';
+
+let inside2='<div id="formula1" class="formula" onclick="showformula(2, 1)">pH</div><div id="formula2" class="formula" onclick="showformula(2, 2)">pOH</div><div id="formula3" class="formula" onclick="showformula(2, 3)">przeliczenie<br>pOH na pH</div><div id="formula4" class="formula" onclick="showformula(2, 4)">Stała dysocjacji kwasu</div><div id="formula5" class="formula"  onclick="showformula(2, 5)">Stała dysocjacji zasady</div><div id="formula6" class="formula" onclick="showformula(2, 6)">Stopień dysocjacji<br>(pełny)</div><div id="formula7" class="formula" onclick="showformula(2, 7)">Zasada upraszczania wzoru &uarr;</div><div id="formula8" class="formula" onclick="showformula(2, 8)">Stopień dysocjacji<br>(uproszczony)</div>';
+
+    let bf21='<span onmouseover="explanation(2, 1, 1);">pH</span>=<span onmouseover="explanation(2, 1, 2);">-log<sub>10</sub></span> <span onmouseover="explanation(2, 1, 3);">[H<sup>+</sup>]</span>';
+
+    let bf22='<span onmouseover="explanation(2, 2, 1);">pOH</span>=<span onmouseover="explanation(2, 2, 2);">-log<sub>10</sub></span> <span onmouseover="explanation(2, 2, 3);">[OH<sup>-</sup>]</span>';
+    
+    let bf23='<span onmouseover="explanation(2, 3, 1);">pH</span>=<span onmouseover="explanation(2, 3, 2);">14</span>-<span onmouseover="explanation(2, 3, 3);">pOH</span>';
+    
+    let bf24='<span onmouseover="explanation(2, 4, 1);">K<sub>a</sub></span>=<span onmouseover="explanation(2, 4, 2);">[H<sup>+</sup>]</span>*<span onmouseover="explanatio(2, 4, 3);">[R<sup>-</sup>]</span>/<span onmouseover="explanation(2, 4, 4);">[HR]</span>';
+    
+    let bf25='<span onmouseover="explanation(2, 5, 1);">K<sub>b</sub></span>=<span onmouseover="explanation(2, 5, 2);">[OH<sup>-</sup>]</span>*<span onmouseover="explanation(2, 5, 3);">[M<sup>+</sup>]</span>/<span onmouseover="explanation(2, 5, 4);">[MOH]</span>';
+    
+    let bf26='<span onmouseover="explanation(2, 6, 1);">K</span>=<span onmouseover="explanation(2, 6, 2);">Cm</span>*<span onmouseover="explanation(2, 6, 3);">&#945;<sup>2</sup></span>/<span onmouseover="explanation(2, 6, 4);">(1-&#945;)</span>';
+    
+    let bf27='<span onmouseover="explanation(2, 7, 1);">&#945;</span><5% lub <span onmouseover="explanation(2, 7, 2);">Cm</span>/<span onmouseover="explanation(2, 7, 3);">K</sup></span>>400';
+    
+    let bf28='<span onmouseover="explanation(2, 8, 1);">K</span>=<span onmouseover="explanation(2, 8, 2);">Cm</span>*<span onmouseover="explanation(2, 8, 3);">&#945;<sup>2</sup></span>';
+
+let inside3='<div id="formula1" class="formula">Iloczyn rozpuszcalności</div><div id="formula2" class="formula">Rozpuszczalnść</div><div id="formula3" class="formula">Iloczyn jonowy wody</div><div id="formula4" class="formula">pH buforu kwaśnego</div><div id="formula5" class="formula">pOH buforu zasadowego</div>';
+
+let inside4='<div id="formula1" class="formula">Stężeniowa stała równowagi</div><div id="formula2" class="formula">Ciśnieniowa stała równowagi</div><div id="formula3" class="formula">Wydajność reakcji</div><div id="formula4" class="formula">Równanie Clapeyrona</div>'; 
+
+let inside5='<div id="formula1" class="formula">Stężenie procentowe masowe</div><div id="formula2" class="formula">Stężenie procentowe molowe</div><div id="formula3" class="formula">Stężenie molowe</div><div id="formula4" class="formula">Stężenie molalne mol/m</div>';
+
+let inside6='<div id="formula1" class="formula">Ilość wiązań<br>podwójnych/pierścieni</div><div id="formula2" class="formula">Skręcalność właściwa</div><div id="formula3" class="formula">Maksymalna ilość diastereoizomerów</div>';
+
+let inside7='<div id="formula1" class="formula">Szybkość reakcji</div><div id="formula2" class="formula">Standartowa entalpia</div><div id="formula3" class="formula">Entalpia swobodna</div><div id="formula4" class="formula">Entropia</div><div id="formula5" class="formula">Równanie Arheniusa</div><div id="formula6" class="formula">Równanie kinetyczne 1szego stopnia</div><div id="formula7" class="formula">Równanie kinetyczne 2giego stopnia</div><div id="formula8" class="formula">Równanie kinetyczne 3ciego stopnia</div><div id="formula9" class="formula">Równanie kinetyczne 4tego stopnia</div>';
+
+//część właściwa po zmiennych
+
+function execute_formulas(subject)
+{
+    switch(subject)
+    {
+        case 1:
+            {
+                
+                document.getElementById("formulas").innerHTML=inside1;
+                break;
+            }
+        case 2:
+            {
+                
+                document.getElementById("formulas").innerHTML=inside2;
+                break;
+            }
+        case 3:
+            {
+                
+                document.getElementById("formulas").innerHTML=inside3;
+                break;
+            }
+        case 4:
+            {
+                document.getElementById("formulas").innerHTML=inside4;
+                break;
+            }
+        case 5:
+            {
+                document.getElementById("formulas").innerHTML=inside5;
+                break;
+            }
+        case 6:
+            {
+                document.getElementById("formulas").innerHTML=inside6;
+                break;
+            }
+        case 7:
+            {
+                document.getElementById("formulas").innerHTML=inside7;
+                break;
+            }
+    }
+}
+
+function showformula(group, place)
+{
+    switch(group)
+    {
+        case 1:
+            {
+                switch(place)
+                {
+                    case 1:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf11;
+                            break;
+                        }
+                    case 2:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf12;
+                            break;
+                        }
+                    case 3:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf13;
+                            break;
+                        }
+                    case 4:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf14;
+                            break;
+                        }
+                }
+            break;
+            }
+        case 2:
+            {
+                switch(place)
+                {    case 1:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf21;
+                            break;
+                        }
+                    case 2:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf22;
+                            break;
+                        }
+                    case 3:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf23;
+                            break;
+                        }
+                    case 4:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf24;
+                            break;
+                        }
+                    case 5:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf25;
+                            break;
+                        }
+                    case 6:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf26;
+                            break;
+                        }
+                    case 7:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf27;
+                            break;
+                        }
+                    case 8:
+                        {
+                            document.getElementById("bigformula").innerHTML=bf28;
+                            break;
+                        }
+                }
+                break;
+            }
+    }
+}
